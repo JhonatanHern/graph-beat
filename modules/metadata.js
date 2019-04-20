0913d95e123970c0827fa502d860108795eedcf7
JhonatanHern <jhonatanhernandez998@gmail.com>
2019-04-20 18:19:12 -0400

    queries finished

--- modules/metadata.js
+++ modules/metadata.js
@@ -1,9 +1,58 @@
-const fs = require('fs')
-const fs = require('fs')
+const fs = require('fs').promises
+
+const globalAppData = require('./globalAppData')
 
 class Metadata{
-	static getArtists(){}
-	static getSongs(artist){}
+	static getArtists(){
+		return new Promise( async (succ,err) => {
+			let data
+			try{
+				data = await fs.readdir(globalAppData.tracksDir)
+			}catch(fileError){
+				console.log("file error: ",fileError)
+				err(fileError)
+				return
+			}
+			succ(data.map(str=>({title:str})))
+		})
+	}
+	static getSongs(args){
+		return new Promise(async (succ,err)=>{
+			let data
+			const artist = Metadata.cleanInput(args.artist)
+			const dir = globalAppData.tracksDir + '/' + artist
+			try{
+				data = await fs.readdir(dir)
+			}catch(fileError){
+				console.log("file error: ",fileError)
+				err(fileError)
+				return
+			}
+			const promises = data.map(entry=>{
+				return new Promise(async (succ,err)=>{
+					const isdir = (await fs.lstat(dir+'/'+entry)).isDirectory()
+					succ( isdir ? entry : false )
+				})
+			})
+			let values = await Promise.all(promises)
+			values = values.filter(v=>v).map(v=>({title:v,songs:[]}))
+			const albumDataPromises = values.map(v=>Metadata.getSongsFromAlbum(v.title,artist,v.songs))
+			await Promise.all(albumDataPromises)
+			succ(values)
+		})
+	}
+	//args: { albumName : String ,}
+	static getSongsFromAlbum(albumName,artist,arrayToPush){
+		return new Promise(async (succ,err)=>{
+			let songs = await fs.readdir(`${globalAppData.tracksDir}/${artist}/${albumName}`)
+			songs = songs.filter(file=>/\.mp3$/.test(file))
+			songs.forEach(s=>arrayToPush.push({title:s}))
+			succ()
+		})
+	}
+	static cleanInput(input){
+		return input.split('/').join('')
+	}
 }
 
 module.exports = Metadata
